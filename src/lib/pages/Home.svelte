852c463107ad359ac9645da659b39ab765edd28b
GrungeElFz <elf0114_@hotmail.com>
2023-03-27 15:42:33 -0500

    Modified:   Added `on:click` that increments `scale` by 1 (src/lib/pages/Home.svelte)             Added `on:click` that decrements `scale` by 1             Added `transform-origin` prop, assigned its value to `0 0`

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -4,8 +4,25 @@
 	const scale = writable(1);
 </script>
 
-<button>Bigger</button>
-<button>Smaller</button>
+<button
+	on:click={() => {
+		$scale += 1;
+	}}
+>
+	Bigger
+</button>
+<button
+	on:click={() => {
+		$scale -= 1;
+	}}
+>
+	Smaller
+</button>
 <div
-	style="width: 7rem; height: 7rem; background-color: rgb(130,115,210); transform: scale({$scale})"
+	style="
+		width: 7rem;
+		height: 7rem;
+		background-color: rgb(130,115,210);
+		transform: scale({$scale});
+		transform-origin: 0 0"
 />
