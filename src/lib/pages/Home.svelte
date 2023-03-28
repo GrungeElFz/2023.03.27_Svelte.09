8797f13af47ffab4268954b779aee771077ad2d5
GrungeElFz <elf0114_@hotmail.com>
2023-03-28 00:53:14 -0500

    Modified:   Changed all the `size` occurences to `boxProps` (src/lib/pages/Home.svelte)             Changed a <button> name 'Random size' to 'Random Box`
                In a function tweened(...)
                - First param, added a prop `color`, assigned its value to 'blue'
                - Second param, added a prop `interpolate`
    
                In a prop `interpolate`
                - Added a function that takes 2 arguments
                - Assigned the first arg (the initial value, the first param of `tweened()`) as `a`
                - Assigned the second arg (the value that will be animated to, the value of `boxProps.set()`) , as `b`
    
                In a function (a, b)
                - Returned to another function `t`
    
                In a function (t)
                - Added a const variable `deltaWidth`, assigned it's value to `b.width - a.width`
                - Added a const variable `deltaHeight`, assigned it's value to `b.height - a.height`
                - Returned 3 props, { width, height, color }
    
                In an await boxProps.set(...)
                - Added a prop `color`, assigned its value to a random color generator
    
                In <div>
                - Replaced the value of a prop `background-color` from `rgb(130,115,210)` to `{$boxProps.color}`

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -2,33 +2,44 @@
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 
-	const size = tweened(
+	const boxProps = tweened(
 		{
 			width: 100,
-			height: 100
+			height: 100,
+			color: 'blue'
 		},
 		{
 			duration: 500,
-			easing: cubicOut
+			easing: cubicOut,
+			interpolate: (a, b) => (t) => {
+				const deltaWidth = b.width - a.width;
+				const deltaHeight = b.height - a.height;
+				return {
+					width: a.width + deltaWidth * t,
+					height: a.height + deltaHeight * t,
+					color: 'orange'
+				};
+			}
 		}
 	);
 </script>
 
 <button
 	on:click={async () => {
-		await size.set({
+		await boxProps.set({
 			width: Math.random() * 500,
-			height: Math.random() * 500
+			height: Math.random() * 500,
+			color: `#${Math.floor(Math.random() * 16777215).toString(16)}`
 		});
 		console.log('Done');
 	}}
 >
-	Random Size
+	Random Box
 </button>
 
 <div
 	style="
-		width: {$size.width}px;
-		height: {$size.height}px;
-		background-color: rgb(130,115,210);"
+		width: {$boxProps.width}px;
+		height: {$boxProps.height}px;
+		background-color: {$boxProps.color};"
 />
