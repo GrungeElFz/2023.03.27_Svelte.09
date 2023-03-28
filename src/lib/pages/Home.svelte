1989dfd543a76f4e254bda928c3fb5f41c8a1707
GrungeElFz <elf0114_@hotmail.com>
2023-03-28 01:37:19 -0500

    Modified:   Imported { interpolateRgb } from 'd3-interpolate' (src/lib/pages/Home.svelte)
                In a function (t)
                - Added a const variable `color`, assigned its value to `interpolateRgb(a.color, b.color)(t)`
                - Removed a prop `color`'s value
    
                In an await function boxProps.set()
                - Added a second option `duration`, assigned its value to 2000
                /* When the function is called, this duration will override any previous options */

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
+	import { interpolateRgb } from 'd3-interpolate';
 
 	const boxProps = tweened(
 		{
@@ -14,10 +15,11 @@
 			interpolate: (a, b) => (t) => {
 				const deltaWidth = b.width - a.width;
 				const deltaHeight = b.height - a.height;
+				const color = interpolateRgb(a.color, b.color)(t);
 				return {
 					width: a.width + deltaWidth * t,
 					height: a.height + deltaHeight * t,
-					color: 'orange'
+					color
 				};
 			}
 		}
@@ -26,11 +28,14 @@
 
 <button
 	on:click={async () => {
-		await boxProps.set({
-			width: Math.random() * 500,
-			height: Math.random() * 500,
-			color: `#${Math.floor(Math.random() * 16777215).toString(16)}`
-		});
+		await boxProps.set(
+			{
+				width: Math.random() * 500,
+				height: Math.random() * 500,
+				color: `#${Math.floor(Math.random() * 16777215).toString(16)}`
+			},
+			{ duration: 2000 }
+		);
 		console.log('Done');
 	}}
 >
